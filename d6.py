b3b78ddf36fe55a237523911ff17ec120637fc88
Topi Tuulensuu <topi@topituulensuu.com>
2021-12-09 20:49:54 +0200

    day6: a non dumb solution
    no running out of memory this time

--- d6.py
+++ d6.py
@@ -4,37 +4,17 @@ data = []
 with open("6.in", "r") as _input:
     data = [int(x) for x in [x.strip().split(",") for x in _input.readlines()][0]]
 
-fishes = set()
-
-class Fish():
-    def __init__(self, day=8):
-        self.day = day
-
-    def cycle(self):
-        day = self.day
-        if day == 0:
-            new_fish = Fish()
-            fishes.add(new_fish)
-            day = 6
-        else:
-            day -= 1
-        self.day = day
-
-    def __str__(self):
-        return str(self.day)
-
-    def __repr__(self):
-        return str(self.day)
+fishes = [0,0,0,0,0,0,0,0,0]
 
 for fish in data:
-    fishes.add(Fish(int(fish)))
+    fishes[fish] += 1
 
 def cycle():
-    tmp_fishes = fishes.copy()
-    for fish in tmp_fishes:
-        fish.cycle()
+    breeding = fishes.pop(0)
+    fishes.append(breeding)
+    fishes[6] += breeding
 
-for _ in range(80):
+for i in range(256):
     cycle()
+print(sum(fishes))
 
-print(len(fishes))
